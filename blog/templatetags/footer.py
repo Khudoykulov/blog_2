361f522646ec84e3a301f593b189fe1713200fa9
Khudoykulov <diyorbekxudoyqulov627@gmail.com>
2024-01-30 11:26:44 +0500

    1

---
+++ blog/templatetags/footer.py
@@ -0,0 +1,9 @@
+from django import template
+from blog.models import AboutMe
+
+
+register = template.Library()
+
+@register.simple_tag
+def footer_author():
+    return AboutMe.objects.order_by('-id')[0]
